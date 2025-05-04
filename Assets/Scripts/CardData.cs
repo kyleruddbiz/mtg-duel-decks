40f0e1fc2913d026e67b1f11303e12b100bf7b28
Kyle Rudd <kyleruddbiz@gmail.com>
2025-05-04 14:26:51 -0600

    Add Card Sprites and CardData (Empty)

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace VoidScribe.MtgDuelDecks
+{
+    public class CardData : ScriptableObject
+    {
+        // Cost
+        // Power and Toughness
+        // Card Type - Creature, Sorcery, Instant, etc.
+        // Subtype - Human, Wizard, Warlock, etc.
+        // Keyword Abilities - such as flying, trample, etc.
+        // Activated abilities and static abilities
+        // Sprite Options
+    }
+}
