f34d043b298a374902dd3e20d19ef7b75b038c4f
Kyle Rudd <kyleruddbiz@gmail.com>
2025-05-05 17:02:56 -0600

    Add shuffling

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -18,32 +18,41 @@ namespace VoidScribe.MtgDuelDecks
 
         public void Initialize()
         {
+            InitializeDeck();
+
+            //Card cardToMove = deckZoneRuntimeSet.Cards[2];
+            //deckZoneRuntimeSet.RemoveCard(cardToMove);
+            //handZoneRuntimeSet.AddCard(cardToMove);
+
+            //cardToMove = deckZoneRuntimeSet.Cards[3 - 1];
+            //deckZoneRuntimeSet.RemoveCard(cardToMove);
+            //handZoneRuntimeSet.AddCard(cardToMove);
+
+            //cardToMove = deckZoneRuntimeSet.Cards[6 - 2];
+            //deckZoneRuntimeSet.RemoveCard(cardToMove);
+            //handZoneRuntimeSet.AddCard(cardToMove);
+        }
+
+        private void InitializeDeck()
+        {
+            var cards = new List<Card>();
+
             foreach (CardSet cardSet in deckData.CardSets)
             {
                 for (int i = 0; i < cardSet.Count; i++)
                 {
                     Card card = Instantiate(cardPrefab);
-
                     card.Initialize(cardSet.CardData, imageIndex: i);
 
-                    deckZoneRuntimeSet.AddCard(card);
-
-                    // todo
-                    //AddCard(card);
+                    int insertIndex = Random.Range(0, cards.Count);
+                    cards.Insert(insertIndex, card);
                 }
             }
 
-            Card cardToMove = deckZoneRuntimeSet.Cards[2];
-            deckZoneRuntimeSet.RemoveCard(cardToMove);
-            handZoneRuntimeSet.AddCard(cardToMove);
-
-            cardToMove = deckZoneRuntimeSet.Cards[3 - 1];
-            deckZoneRuntimeSet.RemoveCard(cardToMove);
-            handZoneRuntimeSet.AddCard(cardToMove);
-
-            cardToMove = deckZoneRuntimeSet.Cards[6 - 2];
-            deckZoneRuntimeSet.RemoveCard(cardToMove);
-            handZoneRuntimeSet.AddCard(cardToMove);
+            foreach (Card card in cards)
+            {
+                deckZoneRuntimeSet.AddCard(card);
+            }
         }
     }
 }
