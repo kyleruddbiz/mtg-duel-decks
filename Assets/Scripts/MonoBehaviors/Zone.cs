7fd52638a5df433bc1b6c322f234d99fb5f4018a
Kyle Rudd <kyleruddbiz@gmail.com>
2025-05-05 11:21:05 -0600

    Super ugly zones for testing.

---
+++ Assets/Scripts/MonoBehaviors/Zone.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace VoidScribe.MtgDuelDecks
+{
+    public class Zone : MonoBehaviour
+    {
+        [SerializeField] private Transform cardsDisplay;
+        [SerializeField] private TextMeshPro zoneNameText;
+        [SerializeField] private string zoneName;
+        [SerializeField] private float horizontalOffset;
+        [SerializeField] private List<Card> cards;
+
+        public void Awake()
+        {
+            zoneNameText.text = zoneName;
+
+            UpdateVisual();
+        }
+
+        private void Update()
+        {
+            // TODO Delete
+            UpdateVisual();
+        }
+
+        private void UpdateVisual()
+        {
+            for (int i = 0; i < cards.Count; i++)
+            {
+                cards[i].transform.SetParent(cardsDisplay);
+                cards[i].transform.localPosition = new Vector3(horizontalOffset * i, 0, -(.01f * i));
+            }
+        }
+    }
+}
