037c499e9750302df125b93a3b8133a2ffecfb29
Kyle Rudd <kyleruddbiz@gmail.com>
2025-05-07 07:39:31 -0600

    Add player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace VoidScribe.MtgDuelDecks
+{
+    [CreateAssetMenu(fileName = "Player", menuName = "VoidScribe/Player")]
+    public class Player : ScriptableObject
+    {
+        [field: SerializeField] public string PlayerName { get; private set; }
+        [field: SerializeField] public DeckData Deck { get; private set; }
+        [field: SerializeField] public Player Opponent { get; private set; }
+        [field: SerializeField] public int Health { get; private set; } = 20;
+    }
+}
