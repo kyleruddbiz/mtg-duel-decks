09f3fa15dc2ad3554fed57c556ef911d0c8f6f46
Kyle Rudd <kyleruddbiz@gmail.com>
2025-05-07 16:55:09 -0600

    Add mana display

---
+++ Assets/Scripts/MonoBehaviors/ManaDisplay.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+namespace VoidScribe.MtgDuelDecks
+{
+    public class ManaDisplay : MonoBehaviour
+    {
+        [Header(HeaderConstants.Internal)]
+        [SerializeField] private SpriteRenderer image;
+        [SerializeField] private TextMeshPro amountText;
+
+        [Header(HeaderConstants.Fields)]
+        [SerializeField] private ManaManager manaManager;
+        [SerializeField] private Color color;
+        [SerializeField] private Sprite symbolSprite;
+
+        private void Awake()
+        {
+            image.sprite = symbolSprite;
+            amountText.text = manaManager.GetAvailableMana(color).ToString();
+        }
+
+        // TODO - Move this out of the update loop
+        private void Update()
+        {
+            amountText.text = manaManager.GetAvailableMana(color).ToString();
+
+            Debug.Log(amountText.text);
+            Debug.Log("managerseL: " + manaManager.GetAvailableMana(color).ToString());
+        }
+    }
+}
